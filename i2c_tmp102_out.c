37362c89d4fab3a2dfb95c89e4f7054b825e8f39
Aurélien Chabot <aurelien@chabot.fr>
2016-04-16 01:34:58 +0200

    Don't print the temp of 0 in case of error

--- i2c_tmp102_out.c
+++ i2c_tmp102_out.c
@@ -4,7 +4,9 @@ int main(int argc, char **argv)
 {
     double temp;
 
-    read_i2c("/dev/i2c-0", 0x48, &temp);
+    if(read_i2c("/dev/i2c-0", 0x48, &temp))
+		return; // failed
+
     //read_i2c("/dev/i2c-0", 0x49, &temp);
 
     printf("%.1f", temp);
